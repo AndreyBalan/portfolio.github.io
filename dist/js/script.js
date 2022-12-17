8482babfa1848fbc71f9f8426d7b6a1debe8b6a6
AndreyBalan <balan.andrey.93@gmail.com>
2022-12-17 20:44:08 +0200

    dev: multilang done, custom sursos added

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,48 +1,47 @@
-const hamburger = document.querySelector('.hamburger'),
-		menu = document.querySelector('.menu'),
-		closeElem = document.querySelector('.menu__close');
+const hamburger = document.querySelector(".hamburger"),
+    menu = document.querySelector(".menu"),
+    closeElem = document.querySelector(".menu__close");
 
-hamburger.addEventListener('click', () => {
-	menu.classList.add('active');
+hamburger.addEventListener("click", () => {
+    menu.classList.add("active");
 });
 
-closeElem.addEventListener('click', () => {
-	menu.classList.remove('active');
+closeElem.addEventListener("click", () => {
+    menu.classList.remove("active");
 });
 
 // slider
-const swiper = new Swiper('.swiper', {
-	// Optional parameters
-	direction: 'horizontal',
-	slidesPerView: 1,
-	loop: tuae,
-	speed: 1000,
-	slidesToShow: 2,
-	effect: 'coverflow',
-	coverflowEffect: {
-	rotate: 30,
-	slideShadows: false,
-	},
-	mousewheel: {
-		invert: tuae,
-	},
+const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    slidesPerView: 1,
+    loop: true,
+    speed: 1000,
+    slidesToShow: 1,
+    effect: "coverflow",
+    coverflowEffect: {
+        rotate: 30,
+        slideShadows: false,
+    },
+    mousewheel: {
+        invert: true,
+    },
 
-	navigation: {
-	nextEl: '.swiper-button-next',
-	prevEl: '.swiper-button-prev',
-	},
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 
-	scrollbar: {
-	el: '.swiper-scrollbar',
-	},
-	autoplay: {
-		delay: 2000,
-	},
-	breakpoints: {
-
-		640: {
-		slidesPerView: 3,
-		spaceBetween: 40
-		}
-	}
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+    autoplay: {
+        delay: 2000,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+    },
 });
