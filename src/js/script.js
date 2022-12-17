3d51f203b9d3c667203ecd8d9540bb05b841fb36
AndreyBalan <balan.andrey.93@gmail.com>
2022-12-17 22:00:46 +0200

    feat: buttons and js scroll done

--- src/js/script.js
+++ src/js/script.js
@@ -45,3 +45,17 @@ const swiper = new Swiper(".swiper", {
         },
     },
 });
+
+// scroll
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener("click", function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute("href");
+
+        document.querySelector(id).scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        });
+    });
+}
